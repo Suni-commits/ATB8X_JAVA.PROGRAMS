64197b322a154e8753f332467d3ee89c8e87d19d
Suneetha <suneetha.la@gmail.com>
2024-09-12 21:53:38 +0530

    updated

---
+++ src/Sept/SeptClass.java
@@ -0,0 +1,12 @@
+package Sept;
+
+public class SeptClass {
+public static void main(String[] args)
+{
+    int x=2;
+    int y=5;
+    System.out.println("This is Integer X:" + x +"\n" +y);
+
+}
+
+}
