2a2915bc6864c0e3611796abeba672bce9941a2a
Suneetha <suneetha.la@gmail.com>
2024-09-12 21:42:16 +0530

    added

---
+++ src/MyClass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//import java.lang.String;
+import java.io.PrintStream;
+
+public class MyClass {
+    public static void main(String[] args)
+{
+    System.out.println("Hello World");
+}
+}
+
+
+
+
---
+++ src/Sept/SecondPro.java
@@ -0,0 +1,12 @@
+package Sept;
+
+public class SecondPro {
+public static void main(String[] args)
+{
+    System.out.println(20+30);
+    System.out.println(30-30);
+    System.out.println(30/30);
+    System.out.println(300%30);
+}
+
+}
