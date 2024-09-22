bb1d432a372939727670b9cb6908442a11307145
Suneetha <suneetha.la@gmail.com>
2024-09-22 14:39:26 +0530

    updated

---
+++ src/Sept/TrianleArea.java
@@ -0,0 +1,23 @@
+package Sept;
+
+public class TrianleArea {
+
+    public static void main(String[] args)
+    {
+        int a = 60;
+        int b = 60;
+        int c = 60;
+        if (a == b && b == c && a == c){
+            System.out.println("This is eaquivalent traingle");
+        }
+        else if ( a>b && c>b && a==c || a==c){
+
+            System.out.println("This is isolosic traingle");
+        }
+            else
+        {
+            System.out.println("This is scale traingle");
+
+        }
+    }
+}
