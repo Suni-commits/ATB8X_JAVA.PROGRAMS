fc90e17dcb7a6ee6593dcca6211f73e4d7101949
Suneetha <suneetha.la@gmail.com>
2024-09-22 17:08:51 +0530

    updated

---
+++ src/Sept/CompareThree.java
@@ -0,0 +1,16 @@
+package Sept;
+
+public class CompareThree {
+    public static void main(String [] args){
+        int a = 10;
+        int b = 20;
+        int c = 45;
+
+        int g= (a > b)
+                ? ((a > c) ? a : c)
+                : ((b > c) ? b : c);
+        System.out.println("The maximum number amoing three nmberds; " + g);
+
+
+    }
+}
