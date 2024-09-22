7775b145aafee2efa68b94ed080b3f76bc7354c2
Suneetha <suneetha.la@gmail.com>
2024-09-22 14:08:40 +0530

    updated

---
+++ src/Sept/Mulitily.java
@@ -0,0 +1,47 @@
+package Sept;
+
+public class Mulitily {
+    public static void main (String[] args){
+        int i = 1;
+        int num=4;
+        for (i=1; i<=10;i++ )
+        {
+            System.out.println("Following is the table of 4");
+                num =num++;
+            System.out.println(i + "*" + num +"=" +i*num);
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
