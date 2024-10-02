ce7904afc2a3322072980a40ffd4eb26467e35b1
Suneetha <suneetha.la@gmail.com>
2024-10-02 13:52:24 +0530

    updated

---
+++ src/Oct/Lab0026.java
@@ -0,0 +1,16 @@
+package Oct;
+
+public class Lab0026 {
+    public static void main(String[] args){
+    //decreasing astrix
+
+       for(int i=5;i>=1; --i){
+
+        for(int j=1;j<=i;++j)
+        {
+            System.out.println("*  ");
+        }
+        System.out.println(    );
+    }
+}
+}
